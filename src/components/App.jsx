bc17aa736fcf8636ddd0395a3a04a00d53c84b49
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-02-22 19:01:31 +0100

    done with layout logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className='flex justify-center min-h-screen my-8'>
       <div className='flex gap-x-6 h-full laptop:max-w-[1200px] tablet:max-w-[810px]'>
         <div className='flex gap-x-6 flex-col tablet:flex-row gap-y-6 mx-4 tablet:mx-0 '>
-          <div className=''>
+          <div className='mx-auto'>
             <About />
           </div>
           <div className=''>
