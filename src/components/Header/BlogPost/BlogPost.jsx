61d252130b64b47c221026248ae524131d336ea3
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-02-24 22:47:42 +0100

    m

---
+++ src/components/Header/BlogPost/BlogPost.jsx
@@ -0,0 +1,8 @@
+function BlogPost(){
+ return (
+   <div className='tablet:bg-dark-100 w-75 md:max-w-75 px-3 py-4 rounded-2xl text-xs tablet:border tablet:border-light-10'>
+   
+   </div>
+ )
+}
+export default BlogPost
